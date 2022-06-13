7c484d35458b96c5f63f93bb9339911fd1562147
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-13 14:13:04 +0200

    FunctionRemoveAdd

--- app.js
+++ app.js
@@ -4,13 +4,17 @@
 // when user clicks close-btn remove .open-modal from modal-overlay
 
 const modalBtn = document.querySelector('.modal-btn');
-const modal = document.querySelector('.modal-overlay');
+const modalOverlay = document.querySelector('.modal-overlay');
 const closeBtn = document.querySelector('.close-btn');
 
-modalBtn.addEventListener('click', function() {
-  modal.classList.add('open-modal');
+modalBtn.addEventListener('click', () => {
+  if (modalOverlay.classList.contains('open-modal')) {
+    modalOverlay.classList.remove('open-modal');
+  } else {
+    modalOverlay.classList.add('open-modal');
+  }
 });
 
-closeBtn.addEventListener('click', function() {
-  modal.classList.remove('open-modal');
+closeBtn.addEventListener('click', () => {
+  modalOverlay.classList.remove('open-modal');
 });
